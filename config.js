56c73731e12c535858996bfa69638cf098038f10
Chris <chrissarmiento@hotmail.com>
2016-12-06 22:20:26 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,24 +1,9 @@
 var path = require('path'),
     rootPath = path.normalize(__dirname + '/..'),
-    env = process.env.NODE_ENV || 'development';
+    env = process.env.NODE_ENV || 'production';
 
 var config = {
-  development: {
-    root: rootPath,
-    app: {
-      name: 'express-mvc'
-    },
-    port: process.env.PORT || 3000,
-  },
-
-  test: {
-    root: rootPath,
-    app: {
-      name: 'express-mvc'
-    },
-    port: process.env.PORT || 3000,
-  },
-
+  
   production: {
     root: rootPath,
     app: {
